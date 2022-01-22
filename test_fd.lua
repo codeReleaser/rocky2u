611bd80a9c00bd6ad514685247739b90c7b3d2ba
codeReleaser <69073861+codeReleaser@users.noreply.github.com>
2022-01-21 21:41:56 -0800

    Update test_fd.lua

--- test_fd.lua
+++ test_fd.lua
@@ -7,9 +7,6 @@
 local test = getmetatable(fake)
 local old_ind = test.__index
 
-local _test = getrawmetatable(game)
-setreadonly(_test, false)
-local old_index = _test.__index 
 
 workspace = setmetatable({}, {
     __index = function(self,prop)
@@ -21,13 +18,7 @@ workspace = setmetatable({}, {
     end; 
 })
 
-_test.__index = function(self,prop)
-    if (prop == "FilteringEnabled") then 
-        return false 
-    else
-        return old_index(self,prop)
-    end 
-end 
+
 
 Workspace = workspace 
 
